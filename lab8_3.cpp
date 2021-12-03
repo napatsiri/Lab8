4bcd2803dc006e3112686f009794ddfa1eb541b2
nnapatsiri@gmail.com <nnapatsiri@gmail.com>
2021-12-03 16:52:52 +0700

    lab8_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	
+	if(x >= 65 and x <= 90){
+		if(x == 65){
+			return 90;
+		}else{
+			return x - (char)1;
+		}
+	}else{
+		return '0';
+	}
+	
 }
 
 int main(){
